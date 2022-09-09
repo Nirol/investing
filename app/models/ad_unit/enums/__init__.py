624afdf9e5cedaa80810bab6e3c7f544b1c034ae
Nirol <nirolgg@gmail.com>
2022-09-09 16:28:08 +0300

    Switched from relative to absolute import.
    Encounter an issue creating migration while using relative imports, converted to absolute import.

--- app/models/ad_unit/enums/__init__.py
+++ app/models/ad_unit/enums/__init__.py
@@ -1,4 +1,4 @@
-from operating_system import OperatingSystem  # noqa
-from device import Device  # noqa
-from browser import Browser  # noqa
-from language import Language  # noqa
+from app.models.ad_unit.enums.operating_system import OperatingSystem  # noqa
+from app.models.ad_unit.enums.device import Device  # noqa
+from app.models.ad_unit.enums.browser import Browser  # noqa
+from app.models.ad_unit.enums.language import Language  # noqa
