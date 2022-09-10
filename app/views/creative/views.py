4e9e9198bd30f3b33ce08bfb6ed51b17d504ee1d
Nirol <nirolgg@gmail.com>
2022-09-10 12:53:38 +0300

    ADD creative PUT endpoint

--- app/views/creative/views.py
+++ app/views/creative/views.py
@@ -54,3 +54,67 @@ def create_creative():
     except IntegrityError or ProgrammingError as error:
         abort(400, error)
     return creative_schema.dump(creative), 201
+
+@creative_bp.route('/creative/<int:creative_id>', methods=['PATCH'])
+def patch_creative(creative_id):
+    if not request.json:
+        abort(400)
+    creative_schema = CreativeSchema()
+
+    if not CreativeSchema.is_patch_fields_valid(data=request.json, updateable_fields=CreativeSchema.CREATIVE_UPDATABLE_FIELDS):
+        abort(400,f"Only the fields {CreativeSchema.CREATIVE_UPDATABLE_FIELDS} are updatable")
+
+    creative = Creative.query.get_or_404(creative_id)
+
+    for key, value in request.json.items():
+        setattr(creative, key, value)
+
+    # validate patched creative values:
+    try:
+        creative_schema.load(creative.updatable_fields_json(updatable_fields=CreativeSchema.CREATIVE_UPDATABLE_FIELDS))
+    except ValidationError as error:
+        abort(400,error.messages)
+
+
+    # update the updated_at field:
+    creative.updated_at = datetime.utcnow()
+
+    try:
+        db.session.commit()
+    except IntegrityError as error:
+        abort(400, error)
+    return creative_schema.dump(creative)
+
+
+
+
+@creative_bp.route('/creative/<int:creative_id>', methods=['PUT'])
+def put_creative(creative_id):
+    if not request.json:
+        abort(400)
+    creative_schema = CreativeSchema()
+
+    try:
+        # as in POST request, the payload need to hold all required line item arguments.
+        creative_schema.load(request.json)
+    except ValidationError as error:
+        abort(400,error.messages)
+
+
+    creative = Creative.query.get_or_404(creative_id)
+
+    for key, value in request.json.items():
+        setattr(creative, key, value)
+
+    # validate patched line item value:
+    try:
+        creative_schema.load(creative.updatable_fields_json(updatable_fields=CreativeSchema.CREATIVE_UPDATABLE_FIELDS))
+    except ValidationError as error:
+        abort(400,error.messages)
+
+
+    # update the updated_at field:
+    creative.updated_at = datetime.utcnow()
+
+    db.session.commit()
+    return creative_schema.dump(creative)
